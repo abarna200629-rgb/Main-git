7749971f7d7216ea0a9b8eac87739c2d2ebf1a4b
24ada01-wq <24ada01@karpagamtech.ac.in>
2026-05-22 10:21:26 +0530

    Added all java classes

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student {
+
+    int id;
+    String name;
+    int mark;
+
+    Student(int id, String name, int mark) {
+        this.id = id;
+        this.name = name;
+        this.mark = mark;
+    }
+}
